0bc21849de0b0b6b40d968c7beb59db6e18f239a
chris <chrisramoutar1@gmail.com>
2015-01-06 05:38:06 +0000

    First commit

---
+++ app/models/authorbook.rb
@@ -0,0 +1,5 @@
+class Authorbook < ActiveRecord::Base
+  
+  belongs_to  :book
+  belongs_to  :author
+end
---
+++ app/models/book.rb
@@ -0,0 +1,7 @@
+class Book < ActiveRecord::Base
+  
+ validates :name, :presence => true
+  
+ has_many  :authorbooks
+ has_many  :authors, :through => :authorbooks
+end
